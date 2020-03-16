3f9937343423c36e254c98755307044d356ac017
Trong Binh <trongbinhnguyen15@gmail.com>
2020-03-16 09:34:59 +0700

    docker success

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  let a = "";
+  axios.get(`http://localhost:8080/users`).then(res => {
+    a = res;
+    console.log("a", a.data);
+  });
+
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.js</code> and save to fuck.
+          Edit <code>src/App.js</code> and save to show.
         </p>
         <a
           className='App-link'
